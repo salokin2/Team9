e55c3ea9391bda4daef3ae8218559f5a751b7e6c
MerricsUniqueUsername <45721586+MerricsUniqueUsername@users.noreply.github.com>
2025-04-14 13:11:57 -0400

    Update init.js

--- init.js
+++ init.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', initEventHandling());
 // Initialize the grid
 const holidays = holidayMap();
 const today = new Date();
+const todayEvents = getTodayEvents(today);
 document.querySelector('#yearInput').value = today.getFullYear();
 document.querySelector('#monthSelect').value = today.getMonth();
 createGrid();
 updateCalendarGrid();
+addTodayEventsToSidebar();
 setViewMode('month');
